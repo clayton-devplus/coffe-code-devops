bd5e23374684a2443ec4f94eba9d160a2e105bea
Clayton Oliveira <claytondba@yahoo.com.br>
2023-05-27 15:03:56 -0300

    first commit

---
+++ Data/DBDataContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoffeCodeDevops.Data
+{
+
+
+    public class DBDataContext : DbContext
+    {
+        public IConfiguration Configuration { get; }
+
+        public DBDataContext()
+        {
+
+        }
+        public IConfigurationRoot GetConfiguration()
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            if (env == "Kubernetes")
+            {
+                var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.Kubernetes.json", optional: true, reloadOnChange: true)
+                                                                                                     .AddEnvironmentVariables();
+                return builder.Build();
+            }
+            else
+            {
+                var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                                                                                                     .AddEnvironmentVariables();
+                return builder.Build();
+            }
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = GetConfiguration();
+            optionsBuilder.UseNpgsql(config.GetConnectionString("Postgres"));
+
+        }
+    }
+
+}
