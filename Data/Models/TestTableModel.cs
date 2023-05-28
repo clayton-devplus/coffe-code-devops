5bb2cd1907f2e869b78edaee14e4e01b86a57d8e
Clayton Oliveira <claytondba@yahoo.com.br>
2023-05-28 20:14:52 -0300

    teste deploy

--- Data/Models/TestTableModel.cs
+++ Data/Models/TestTableModel.cs
@@ -2,9 +2,9 @@
 {
     public class TestTableModel
     {
-        public string tableId { get; set; }
-        public string tableName { get; set; }
-        public string tableType { get; set; }
+        public string? tableId { get; set; }
+        public string? tableName { get; set; }
+        public string? tableType { get; set; }
 
     }
 }
