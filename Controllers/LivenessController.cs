697b4a863068b1dbdbfafe4d82a66438b9a14615
Clayton Oliveira <claytondba@yahoo.com.br>
2023-05-28 19:02:54 -0300

    endpoint liveness..

---
+++ Controllers/LivenessController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeCodeDevops.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class LivenessController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
